4fed1572476ee8f24d4a2ae3ec0c63e270e6d996
Hritik <Hritik@HComputer.local>
2018-08-06 14:28:47 +0530

    initial commmit

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+"""loops"""
+n = (int(input()))
+    if n % 5 ==1 and 10 <= n <= 100:
+    print ('You correct dougg')
+    elif n % 2 == 0 and 0 < n <= 9:
+    print ('you was wrong try again b')
+    else :
+    print ('dang b')
