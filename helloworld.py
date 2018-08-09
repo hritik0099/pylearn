75dac6676eebf083d2de2eae504496f4bf3a66af
Hritik <hritik.abrol@gmail.com>
2018-08-09 13:12:20 +0530

    added abc

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,6 @@
-"""loops"""
-n = (int(input()))
-    if n % 5 ==1 and 10 <= n <= 100:
-    print ('You correct dougg')
-    elif n % 2 == 0 and 0 < n <= 9:
-    print ('you was wrong try again b')
-    else :
-    print ('dang b')
+i = 1
+a = 5
+for i in range (1,5):
+    print (1*3)
+
+
